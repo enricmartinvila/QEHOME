65da6d4fee600c2666cfa53ee92b549590a198cc
enricMercura <emv@mercura.dk>
2025-11-13 14:31:33 +0100

    delete unused

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -2,22 +2,22 @@ import { useI18n } from "../Components/i18nContext"
 import { ScrollCTA } from "../Components/ScrollCTA"
 
 export default function MainPage() {
-	const { translations } = useI18n()
+    const { translations } = useI18n()
 
-	return (
-		<div>
-			<section
-				className="h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center text-white"
-				style={{ backgroundImage: `url(${"/images/menjador12.webp"})` }}
-			>
-				<h1 className="text-6xl font-bold drop-shadow-lg text-center z-[10]">
-					{translations.maintexts.text}
-				</h1>
-				<p className="text-xl mt-4 drop-shadow-md text-center">
-					{translations.maintexts.disfruta}
-				</p>
-				<ScrollCTA />
-			</section>
-		</div>
-	)
+    return (
+        <div>
+            <section
+                className="h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center text-white"
+                style={{ backgroundImage: `url(${"/images/menjador12.webp"})` }}
+            >
+                <h1 className="text-6xl font-bold drop-shadow-lg text-center z-[10]">
+                    {translations.maintexts.text}
+                </h1>
+                <p className="text-xl mt-4 drop-shadow-md text-center">
+                    {translations.maintexts.disfruta}
+                </p>
+                <ScrollCTA />
+            </section>
+        </div>
+    )
 }
